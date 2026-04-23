22656f36e88edcd115157772be1cbf4203a569a7
Yunus Ahmed <yunus.20240816@iit.ac.lk>
2026-04-23 23:00:14 +0530

    Create a Configuration file for Communication

---
+++ src/main/java/com/smartcampus/config/JacksonConfig.java
@@ -0,0 +1,25 @@
+package com.smartcampus.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class JacksonConfig implements ContextResolver<ObjectMapper> {
+
+    private final ObjectMapper mapper;
+
+    public JacksonConfig() {
+        mapper = new ObjectMapper();
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        return mapper;
+    }
+}
